608da2d38e2fb3518dde983f5b891fd280c5d73c
rndmking <rgadeyev@gmail.com>
2023-01-08 01:54:56 +0300

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Роберт Гадеев on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
